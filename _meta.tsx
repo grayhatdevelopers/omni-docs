d9866ab4bb1697e264ed5a19939558741dd0c16d
Syed Zohair Abbas Hadi <60155240+NasirAbdullahSyed@users.noreply.github.com>
2023-12-21 17:06:51 +0500

    feat: add blogs view

---
+++ _meta.tsx
@@ -0,0 +1,17 @@
+export default {
+    'index': {
+        title: 'Introduction'
+    },
+    'try-us-out': {
+        title: 'Try Us Out'
+    },
+    'readme': {
+        title: 'Open Source'
+    },
+    '---': {
+        type: 'separator'
+    },
+    'blog': {
+        title: 'Blog',
+    }
+}
---
+++ blog/_meta.tsx
@@ -0,0 +1,9 @@
+export default {
+    'automatons-and-ai-for-javascript-junkies': {
+        title: 'Automatons and AI for JavaScript Junkies',
+    },
+    'sign-in-with-adobe-in-a-next-js-app': {
+        title: 'Sign in with Adobe in a Next.js App',
+    },
+    
+}
