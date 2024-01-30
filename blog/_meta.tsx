aa33c63c6979308afb077e496b8e2717c127802a
Saad A. Bazaz <saadbazaz@hotmail.com>
2024-01-31 03:46:42 +0500

    fix: dub link on homepage

--- blog/_meta.tsx
+++ blog/_meta.tsx
@@ -1,11 +1,11 @@
 export default {
+    'omni-at-future-fest': {
+        title: `Omni at Future Fest`
+    },
     'automatons-and-ai-for-javascript-junkies': {
         title: 'Automatons and AI for JavaScript Junkies',
     },
     'sign-in-with-adobe-in-a-next-js-app': {
         title: 'Sign in with Adobe in a Next.js App',
-    },
-    'omni-at-future-fest': {
-        title: `Omni at Future Fest`
     },
 }
