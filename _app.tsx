08b70717ad21bbcadf847a3cda81f64a85c182a2
Saad A. Bazaz <saadbazaz@hotmail.com>
2024-01-30 23:32:25 +0500

    feat: add better docs indexes

--- _app.tsx
+++ _app.tsx
@@ -1,3 +1,5 @@
+import './index.css'
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- _meta.tsx
+++ _meta.tsx
@@ -1,20 +1,68 @@
+import SidebarTitleComponent from "../components/SidebarTitleComponent";
+
 export default {
+    /* Top right links */
+    "home": {
+        "title": "Home",
+        "type": "page",
+        "href": "https://getomni.app"
+      },
+      "api": {
+        "title": "API ↗",
+        "type": "page",
+        "href": "https://getomni.app/api",
+        "newWindow": true
+      },
+
+
     'index': {
-        title: 'Introduction'
+        title: 'Introduction',
+        "theme": {
+            "layout": "full"
+          }
     },
     'readme': {
         title: 'Open Source'
     },
-    '-': {
-        type: 'separator'
+    '-- For users': {
+        type: 'separator',
+        "title": <SidebarTitleComponent>For users</SidebarTitleComponent>
     },
     'guide': {
-        title: 'Guides',
+        title: 'Guides for integrations',
     },
+    'creatives': {
+        title: 'Creative suite',
+    },
+    '-- Commercial use': {
+        type: 'separator',
+        "title":  <SidebarTitleComponent>Commercial use</SidebarTitleComponent>
+    },
+    'use-cases': {
+        title: 'Use cases',
+    },
+    'pricing': {
+        title: 'Pricing',
+    },
+    '-- For developers': {
+        type: 'separator',
+        "title": <SidebarTitleComponent>For developers</SidebarTitleComponent>
+    },
+    'API': {
+        title: 'API',
+    },
+    'examples': {
+        title: 'Examples',
+    },
+    
+
     '--': {
-        type: 'separator'
+        type: 'separator',
     },
     'blog': {
         title: 'Blogs',
-    }
+    },
+
+
+    
 }
